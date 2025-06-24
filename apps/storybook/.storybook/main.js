025a0de9856fa01fa72f32bed0af72ff9019a868
joao lima <joao.lima@zanthus.com.br>
2025-06-24 15:15:52 -0300

    feat: Add new Avatar component with styles and stories
    - Implemented Avatar component with customizable size and icon.
    - Added Avatar styles in Avatar.scss.
    - Created Avatar stories for Storybook showcasing different states.
    
    feat: Introduce Button component with various styles and functionalities
    
    - Developed Button component supporting filled, outlined, and text variants.
    - Integrated icon support with customizable positions.
    - Added Button styles in Button.scss.
    - Created Button stories for Storybook demonstrating different variants and sizes.
    
    feat: Create CustomLink component for navigation with button styles
    
    - Implemented CustomLink component with support for icons and variants.
    - Added CustomLink styles in CustomLink.scss.
    - Created CustomLink stories for Storybook showcasing different states.
    
    feat: Add global tokens for colors, spacing, typography, and borders
    
    - Introduced tokens in index.scss for consistent styling across components.
    - Organized tokens into respective categories for better maintainability.
    
    feat: Add SVG assets for tutorials and YouTube icons
    
    - Included tutorials.svg and youtube.svg assets in the Storybook assets directory.
    
    style: Add CSS styles for Storybook layout and components
    
    - Created button.css, header.css, and page.css for consistent styling in Storybook.

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,11 +1,8 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-
 const config = {
   stories: [
-    "../packages/docs/storybook/stories/**/*.mdx",
-    "../packages/components/**/*.stories.@(js|jsx|tsx)"
-    // "..//**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    
+    "../stories/**/*.mdx",
+    "../../../packages/components-react/src/**/*.stories.@(js|jsx|tsx)",
   ],
   addons: [
     "@chromatic-com/storybook",
