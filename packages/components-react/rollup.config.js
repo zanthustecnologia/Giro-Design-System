ab23d9409bde55eae5ee0cb502f1156060241980
joao lima <joao.lima@zanthus.com.br>
2025-08-18 22:08:02 -0300

    feat: update Babel and Rollup configurations for improved compatibility and performance

--- packages/components-react/rollup.config.js
+++ packages/components-react/rollup.config.js
@@ -3,54 +3,50 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import packageJson from './package.json' assert { type: 'json' };
-import ignore from 'rollup-plugin-ignore';
+
 export default [
-    {
-        input: 'src/index.tsx',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            peerDepsExternal(),
-            resolve({
-                extensions: ['.tsx', '.jsx', '.ts', '.js'],
-            }),
-            commonjs(),
-            babel({
-                extensions: ['.tsx', '.ts', '.jsx', '.js'],
-                babelHelpers: 'bundled',
-                exclude: 'node_modules/**',
-            }),
-            postcss({
-                extract: 'styles.css',
-                inject: false,
-                minimize: true,
-                sourceMap: true,
-            }),
-            typescript({
-                tsconfig: './tsconfig.json',
-            }),
-        ],
-        external: ['react', 'react-dom'],
-    },
-    {
-        input: 'src/index.tsx',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-        plugins: [
-            ignore(['**/*.scss', '**/*.css']),
-            dts()
-        ],
-    },
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      typescript({
+        tsconfig: './tsconfig.json'
+      }),
+      resolve({
+        extensions: ['.tsx', '.jsx', '.ts']
+      }),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+      }),
+      postcss({
+        inject: true,
+        extract: false,
+        minimize: true,
+        sourceMap: false,
+      }),
+    ],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'src/index.tsx',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
 ];
