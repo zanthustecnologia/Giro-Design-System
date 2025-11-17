a8095cbd07733eed95493fe7f23205f94338da9c
joao lima <joao.lima@zanthus.com.br>
2025-11-17 19:43:15 -0300

    feat: add MenuRadix component with dropdown functionality and styles

---
+++ packages/components-react/src/components/MenuRadix/MenuRadix.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import MenuRadix from './MenuRadix';
+
+const meta: Meta = {
+  component: MenuRadix,
+  title: 'Components/MenuRadix'
+}
+export default meta;
+
