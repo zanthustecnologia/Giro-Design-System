d159d90c9b34214b876e8ee7fac49ab57ef93d8f
felipe.joaquim <felipe.joaquim@zanthus.com.br>
2025-11-24 14:35:05 -0300

    feat: remove unused docs-layout styles and update layout in preview and Welcome components for improved consistency

--- apps/storybook-react/.storybook/preview.jsx
+++ apps/storybook-react/.storybook/preview.jsx
@@ -2,7 +2,6 @@ import { Theme } from '@radix-ui/themes';
 import '../../../packages/tokens/build/css/tokens.css';
 import '../../../packages/react/dist/styles.css';
 import '../src/styles/globals.scss';
-import './docs-layout.scss';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
