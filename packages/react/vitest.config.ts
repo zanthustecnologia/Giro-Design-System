d2372634c7a24807e3e0cdf97b836f9436aded43
Felipe Falcone <felipe.falcone@zanthus.com.br>
2025-12-16 15:25:26 -0300

    feat(tests): add Vitest configuration and setup for React package
    - Created vitest.config.ts to configure Vitest with jsdom environment, coverage reporting, and path aliasing.
    - Added vitest.setup.ts to include custom Jest matchers from Testing Library.
    - Removed non-functional tests

---
+++ packages/react/vitest.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/mockData.ts',
+        '**/*.types.ts',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
