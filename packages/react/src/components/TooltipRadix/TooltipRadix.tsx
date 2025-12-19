79bf420c24f42e0627f5dca00ecac3c0bd0cfce7
Giovani Ramos <giovani.ramos@zanthus.com.br>
2025-12-19 16:57:20 -0300

    refactor: code refactoring after code review

--- packages/react/src/components/TooltipRadix/TooltipRadix.tsx
+++ packages/react/src/components/TooltipRadix/TooltipRadix.tsx
@@ -1,90 +1,36 @@
-import React, { useEffect, useId, useRef, useState } from 'react';
+import React from 'react';
 import styles from './TooltipRadix.module.scss';
 import { Tooltip } from "radix-ui";
 import { TooltipRadixProps } from './TooltipRadix.types';
-import clsx from 'clsx';
 
-const TooltipRadix: React.FC<TooltipRadixProps> = ({ id, text, children, position = 'top-right' }) => {
-	const [visible, setVisible] = useState(false);
-	const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
-	const tooltipRadixRef = useRef<HTMLDivElement | null>(null);
-	const tooltipRadixId = id || useId();
 
-	//Função para quando o mouse estar em cima do icone
-	const handleMouseEnter = () => {
-		if (timeoutRef.current) clearTimeout(timeoutRef.current);
-		setVisible(true);
-  	};
-
-	//Função para quando o mouse sair de cima do icone
-	const handleMouseLeave = (): void => {
-		timeoutRef.current = setTimeout(() => {
-			setVisible(false);
-		}, 800);
-	};
-
-	//Função para ativar pelas teclas
-	const handleKeyDown = (e: React.KeyboardEvent) => {
-		switch (e.key) {
-			case 'Escape':
-				setVisible(false);
-				break;
-			case 'Enter':
-			case ' ': 
-				e.preventDefault();
-				setVisible(!visible);
-				break;
-			default:
-				break;
-		}
-	};
-
-	//Aplicação dos estilos com o clsx
-	const tooltipRadixClass = clsx(
-		styles['zds-tooltip__content'],
-		styles[`zds-tooltip__${position}`]
-	)
-
-	//Limpando o timeoutRef
-	//Ver com o Falcone se essa função não é redundante já que no handleMouseEnter ele já faz o clearTimeout
-	useEffect(() => {
-		return () => {
-			if (timeoutRef.current) {
-				clearTimeout(timeoutRef.current);
-			}
-		};
-	}, []);
-	
-	//TolltipRadix
+const TooltipRadix: React.FC<TooltipRadixProps> = ({
+	children,
+	text ,
+	side = 'bottom',
+	align = 'start',
+	sideOffset = 10,
+	alignOffset = 10
+}) => {
 	return (
-		<Tooltip.Provider>
+		<Tooltip.Provider >
 			<Tooltip.Root>
 				<Tooltip.Trigger asChild>
-					<div
-						className={clsx(styles['zds-tooltip__wrapper'])}
-						onMouseEnter={handleMouseEnter}
-						onMouseLeave={handleMouseLeave}
-						onFocus={handleMouseEnter}
-						onBlur={handleMouseLeave}
-						onKeyDown={handleKeyDown}
-						tabIndex={0}
-						aria-describedby={visible ? tooltipRadixId : undefined}
-					>
+					<div>
 						{children}
-						{visible && (
-							<div
-							ref={tooltipRadixRef}
-							className={tooltipRadixClass}
-							role='tooltip'
-							id={tooltipRadixId}
-							aria-describedby={tooltipRadixId}
-							aria-hidden={!visible}
-							>
-								{text}
-							</div>
-						)}
 					</div>
 				</Tooltip.Trigger>
+				<Tooltip.Portal>
+					<Tooltip.Content
+						className={styles['tooltipContent']}
+						side={side}
+						align={align}
+						sideOffset={sideOffset}
+						alignOffset={alignOffset}
+					>
+						{text}
+					</Tooltip.Content>
+				</Tooltip.Portal>
 			</Tooltip.Root>
 		</Tooltip.Provider>
 	);
--- packages/react/src/components/TooltipRadix/TooltipRadix.types.ts
+++ packages/react/src/components/TooltipRadix/TooltipRadix.types.ts
@@ -4,5 +4,9 @@ export interface TooltipRadixProps {
   id?: string;
   text: React.ReactNode;
   position?: 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left' | 'left' | 'right';
+  side?: "top" | "right" | "bottom" | "left"
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
   children: React.ReactNode;
 }
