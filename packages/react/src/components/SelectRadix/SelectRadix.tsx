c4ce86903dd135330757062f49b28323335823ee
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-01-13 09:43:00 -0300

    refactor: leaving the tooltip disabled by default in Select

--- packages/react/src/components/SelectRadix/SelectRadix.tsx
+++ packages/react/src/components/SelectRadix/SelectRadix.tsx
@@ -31,7 +31,7 @@ const SelectRadix: React.FC<SelectRadixProps> = ({
   'aria-label': ariaLabel,
   'data-testid': testId,
   // Props para label com tooltip
-  tooltip = true,
+  tooltip = false,
   tooltipText,
   side = 'bottom',
 	align = 'start',
