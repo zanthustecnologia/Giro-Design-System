6f0f17d34248f7072bd0e4d4a94501104213fbdd
felipe.joaquim <felipe.joaquim@zanthus.com.br>
2026-01-15 14:58:34 -0300

    refactor(Button): update rules for button usage and improve descriptions

--- apps/storybook-react/src/internal-components/mdx/Rules/Rules.tsx
+++ apps/storybook-react/src/internal-components/mdx/Rules/Rules.tsx
@@ -24,25 +24,25 @@ export const ComponentRules: React.FC<ComponentRulesProps> = ({ rules }) => {
     <div className={styles.componentRules}>
       {rules.map((rule, index) => (
         <div key={index} className={styles.ruleRow}>
-          <div className={`${styles.ruleItem} ${styles.positive}`}>
-            <div className={styles.ruleColumn}>
-              <div className={styles.ruleExample}>{rule.positive.component}</div>
-            </div>
-            <div className={styles.ruleHeader}>
-              <CheckboxChecked24Filled className={styles.icon} />
-              <strong>Do</strong>
-            </div>
-            <p className={styles.ruleDescription}>{rule.positive.description}</p>
-          </div>
           <div className={`${styles.ruleItem} ${styles.negative}`}>
             <div className={styles.ruleColumn}>
               <div className={styles.ruleExample}>{rule.negative.component}</div>
             </div>
             <div className={styles.ruleHeader}>
               <DismissSquare24Filled className={styles.icon} />
-              <strong>Don't</strong>
+              <strong>Não recomendado</strong>
             </div>
             <p className={styles.ruleDescription}>{rule.negative.description}</p>
+          </div>
+          <div className={`${styles.ruleItem} ${styles.positive}`}>
+            <div className={styles.ruleColumn}>
+              <div className={styles.ruleExample}>{rule.positive.component}</div>
+            </div>
+            <div className={styles.ruleHeader}>
+              <CheckboxChecked24Filled className={styles.icon} />
+              <strong>Recomendado</strong>
+            </div>
+            <p className={styles.ruleDescription}>{rule.positive.description}</p>
           </div>
         </div>
       ))}
