a03bec3c67c4056150ba8d4146073efaa9a0c3d5
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-04 16:57:46 -0300

    refactor: Comments removed, import order and ESLint adjusted.

--- packages/react/src/components/Quantity/Quantity.types.ts
+++ packages/react/src/components/Quantity/Quantity.types.ts
@@ -4,22 +4,14 @@ export interface QuantityProps extends Omit<
   React.HTMLAttributes<HTMLDivElement>,
   'onChange'
 > {
-  /** Valor padrão inicial */
   defaultValue?: number;
-  /** Valor controlado externamente */
   value?: number;
-  /** Callback chamado quando o valor muda */
   onChange?: (value: number) => void;
-  /** Define se o componente está desabilitado */
   disabled?: boolean;
-  /** Define se o valor do input será decimal ou inteiro */
   decimal?: boolean;
-  /** Define o tamanho do componente */
   size?: 'lg' | 'sm';
-  /** Define o número de casas decimais quando decimal for true */
   decimalPlaces?: number;
   step?: number;
   id?: string;
-  /** ClassName adicional para customização */
   className?: string;
 }
