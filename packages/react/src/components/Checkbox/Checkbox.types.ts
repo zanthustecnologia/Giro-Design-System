3eff0f4661c0831efc62b09aeda1011dbf80799b
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-04 11:03:10 -0300

    refactor: Applying spreadProps to the Checkbox

--- packages/react/src/components/Checkbox/Checkbox.types.ts
+++ packages/react/src/components/Checkbox/Checkbox.types.ts
@@ -1,4 +1,10 @@
-export interface CheckboxProps {
+import { Checkbox as CheckboxPrimitive } from 'radix-ui';
+import * as React from 'react';
+
+export interface CheckboxProps extends Omit<
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>,
+  'checked' | 'onCheckedChange' | 'disabled' | 'id'
+> {
   id?: string;
   label?: React.ReactNode;
   onCheckedChange?: (checked: boolean) => void;
