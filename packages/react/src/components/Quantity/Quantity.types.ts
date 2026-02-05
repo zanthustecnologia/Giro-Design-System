625415767f1bc4413807322c4f8c69d2e94bf171
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-05 14:52:33 -0300

    refactor: Remove unnecessary comments from QuantityProps interface

--- packages/react/src/components/Quantity/Quantity.types.ts
+++ packages/react/src/components/Quantity/Quantity.types.ts
@@ -1,22 +1,14 @@
 import { Size, BaseProps } from '../../types/common.types';
 
 export interface QuantityProps {
-  /** Valor padrão inicial */
   defaultValue?: number;
-  /** Valor controlado externamente */
   value?: number;
-  /** Callback chamado quando o valor muda */
   onChange?: (value: number) => void;
-  /** Define se o componente está desabilitado */
   disabled?: BaseProps['disabled'];
-  /** Define se o valor do input será decimal ou inteiro */
   decimal?: boolean;
-  /** Define o tamanho do componente */
   size?: Size;
-  /** Define o número de casas decimais quando decimal for true */
   decimalPlaces?: number;
   step?: number;
   id?: BaseProps['id'];
-  /** ClassName adicional para customização */
   className?: BaseProps['className'];
 }
