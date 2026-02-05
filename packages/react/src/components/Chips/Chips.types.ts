9bafeaf42c9365f0a466726e07b4e84d8577e645
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-05 14:54:30 -0300

    refactor: Update ChipsProps to use common types of BaseProps.

--- packages/react/src/components/Chips/Chips.types.ts
+++ packages/react/src/components/Chips/Chips.types.ts
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { TextVariant, BaseProps } from '../../types/common.types';
+
 export interface ChipsProps {
   /** Variante a ser escolhida para utilizar padrões de estilizações pré-definidos */
-  type?: 'neutral' | 'brand' | 'color' | 'success' | 'alert';
+  type?: TextVariant;
   /** Texto a ser exibido dentro do componente */
   title: string;
   /** Ícone react que ficará posicionado à esquerda no componente */
@@ -10,9 +12,9 @@ export interface ChipsProps {
   /** Ícone react que ficará posicionado à direita no componente */
   rightIcon?: React.ReactNode;
   /** Estado alterável para desabilitar */
-  disabled?: boolean;
+  disabled?: BaseProps['disabled'];
   /** Classe CSS adicional */
-  className?: string;
+  className?: BaseProps['className'];
   /** Props adicionais para o elemento div */
   [key: string]: any;
 }
