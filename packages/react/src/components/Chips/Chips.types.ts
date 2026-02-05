a84746505a2e9edb485021c4254252d388fe9159
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-05 14:54:49 -0300

    refactor: Remove unnecessary comments from ChipsProps interface

--- packages/react/src/components/Chips/Chips.types.ts
+++ packages/react/src/components/Chips/Chips.types.ts
@@ -3,18 +3,11 @@ import React from 'react';
 import { TextVariant, BaseProps } from '../../types/common.types';
 
 export interface ChipsProps {
-  /** Variante a ser escolhida para utilizar padrões de estilizações pré-definidos */
   type?: TextVariant;
-  /** Texto a ser exibido dentro do componente */
   title: string;
-  /** Ícone react que ficará posicionado à esquerda no componente */
   leftIcon?: React.ReactNode;
-  /** Ícone react que ficará posicionado à direita no componente */
   rightIcon?: React.ReactNode;
-  /** Estado alterável para desabilitar */
   disabled?: BaseProps['disabled'];
-  /** Classe CSS adicional */
   className?: BaseProps['className'];
-  /** Props adicionais para o elemento div */
   [key: string]: any;
 }
