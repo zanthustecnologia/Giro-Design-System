4492d7a674e13da6ba35d16eba9c9c176e05a4e5
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-05 11:05:33 -0300

    refactor: creation of common types

---
+++ packages/react/src/types/common.types.ts
@@ -0,0 +1,22 @@
+
+export type Size = 'sm' | 'lg';
+
+export type Variant = 'filled' | 'outlined' | 'text';
+
+export type SemanticVariant = 'neutral' | 'brand' | 'color' | 'success' | 'alert';
+
+export type Side = 'top' | 'right' | 'bottom' | 'left';
+
+export type Align = 'start' | 'center' | 'end';
+
+export type IconPosition = 'left' | 'right' | 'both';
+
+export type Locale = 'pt-br' | 'en-us';
+
+export interface BaseProps {
+  id?: string;
+  className?: string;
+  disabled?: boolean;
+}
+
+
---
+++ packages/react/src/types/index.ts
@@ -0,0 +1,2 @@
+
+export type * from './common.types';
