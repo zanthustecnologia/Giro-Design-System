4c14301d867e95ff113e4144ea51e992c884050e
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-05 15:57:33 -0300

    refactor: Remove unnecessary comments from FilterProps interface

--- packages/react/src/components/Filter/Filter.types.ts
+++ packages/react/src/components/Filter/Filter.types.ts
@@ -4,44 +4,24 @@ import { DropdownItem, DropdownType } from '../Dropdown/Dropdown.types';
 import { Variant, BaseProps, Locale } from '../../types/common.types';
 
 export interface FilterProps {
-  /** Items para o dropdown */
   items?: DropdownItem[];
-  /** Tipo do dropdown */
   type?: DropdownType | 'calendar';
-  /** IDs selecionados */
   selectedIds?: string[];
-  /** Callback quando aplicar filtros */
   onApplyFilter?: (selectedIds: string[]) => void;
-  /** Placeholder do dropdown */
   placeholder?: string;
-  /** Habilita busca no dropdown */
   enableSearch?: boolean;
-  /** Texto do botão do filtro */
   buttonText?: string | ReactNode;
-  /** Ícone do botão */
   icon?: ReactElement;
-  /** Variante do botão */
   variant?: Variant;
-  /** Callback chamado quando o filtro é aberto */
   onOpen?: () => void;
-  /** Callback chamado quando o filtro é fechado */
   onClose?: () => void;
-  /** Posição do dropdown */
   position?: 'left' | 'right';
-  /** Se o filtro está desabilitado */
   disabled?: BaseProps['disabled'];
-  /** Classes CSS adicionais */
   className?: BaseProps['className'];
-  /** Data selecionada (quando type='calendar') */
   selectedDate?: Date | null;
-  /** Callback quando data é selecionada */
   onDateSelect?: (date: Date) => void;
-  /** Callback para limpar data */
   onClearDate?: () => void;
-  /** Data mínima permitida */
   minDate?: Date;
-  /** Data máxima permitida */
   maxDate?: Date;
-  /** Locale do calendar */
   locale?: Locale;
 }
