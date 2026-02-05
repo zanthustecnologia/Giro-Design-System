538127d1749b16d0e0961200e97806193f653cad
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-05 15:00:58 -0300

    refactor: Remove unnecessary comments from the CalloutProps interface.

--- packages/react/src/components/Callout/Callout.types.ts
+++ packages/react/src/components/Callout/Callout.types.ts
@@ -3,18 +3,11 @@ import React from 'react';
 import { TextVariant, BaseProps } from '../../types/common.types';
 
 export interface CalloutProps {
-  /** Define o tipo de callout */
   type?: TextVariant;
-  /** Define o título com mais destaque */
   title?: string | null;
-  /** Define o texto com menos destaque */
   text?: string;
-  /** Define o ícone a ser importado */
   icon?: React.ReactNode;
-  /** Define a classe CSS adicional */
   className?: BaseProps['className'];
-  /** Define o id do callout */
   id?: BaseProps['id'];
-  /** Props adicionais para o elemento div */
   [key: string]: any;
 }
