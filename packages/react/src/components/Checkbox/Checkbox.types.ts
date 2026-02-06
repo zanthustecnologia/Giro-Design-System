35ce306b42d2c96f476862289cdbf71837d73288
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-06 15:41:18 -0300

    refactor: TSDocks applied to the Checkbox component.

--- packages/react/src/components/Checkbox/Checkbox.types.ts
+++ packages/react/src/components/Checkbox/Checkbox.types.ts
@@ -1,10 +1,49 @@
+import React from 'react';
+
+import { BaseProps } from '../../types/common.types';
+
+/**
+ * Props do componente Checkbox
+ * @example
+ * ```tsx
+ * <Checkbox 
+ *   label="Aceito os termos"
+ *   checked={accepted}
+ *   onCheckedChange={setAccepted}
+ * />
+ * ```
+ * @example
+ * ```tsx
+ * <Checkbox 
+ *   label="Selecionar todos"
+ *   indeterminate={someSelected}
+ *   onCheckedChange={handleSelectAll}
+ *   disabled={isLoading}
+ * />
+ * ```
+ */
 export interface CheckboxProps {
-  id?: string;
+  /** ID único do elemento */
+  id?: BaseProps['id'];
+  
+  /** Label ou conteúdo a ser exibido ao lado do checkbox */
   label?: React.ReactNode;
+  
+  /** Callback executado quando o estado muda: (checked) => void */
   onCheckedChange?: (checked: boolean) => void;
+  
+  /** Estado inicial (modo não controlado) */
   defaultChecked?: boolean;
+  
+  /** Estado atual (modo controlado) */
   checked?: boolean;
-  disabled?: boolean;
-  className?: string;
+  
+  /** Estado desabilitado do checkbox */
+  disabled?: BaseProps['disabled'];
+  
+  /** Classe CSS customizada */
+  className?: BaseProps['className'];
+  
+  /** Estado indeterminado (usado em selecionar todos com seleção parcial) */
   indeterminate?: boolean;
 }
