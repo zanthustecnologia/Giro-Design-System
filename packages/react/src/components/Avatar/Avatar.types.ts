b56b0a81c7646104451b4c30c5660ccd531b689e
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-06 11:42:26 -0300

    refactro: TSDocks applied to the avatar.

--- packages/react/src/components/Avatar/Avatar.types.ts
+++ packages/react/src/components/Avatar/Avatar.types.ts
@@ -1,8 +1,30 @@
 import React from 'react';
 
+/**
+ * Props do componente Avatar
+ * @example
+ * ```tsx
+ * <Avatar icon={<UserIcon />} size="large" />
+ * ```
+ * @example
+ * ```tsx
+ * <Avatar 
+ *   icon={<ProfileIcon />} 
+ *   size="small"
+ *   className="custom-avatar"
+ * />
+ * ```
+ */
 export interface AvatarProps {
+  /** ID único do elemento */
   id?: string;
+  
+  /** Ícone ou conteúdo a ser exibido no avatar */
   icon: React.ReactNode;
+  
+  /** Tamanho do avatar */
   size?: 'small' | 'large';
+  
+  /** Classe CSS customizada */
   className?: string;
 }
