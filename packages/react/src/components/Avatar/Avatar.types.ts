234f3dd91e2fdc96aaab43f326519d87138e5f9c
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-06 15:31:49 -0300

    refactor: TSDocks applied to the Avatar component.

--- packages/react/src/components/Avatar/Avatar.types.ts
+++ packages/react/src/components/Avatar/Avatar.types.ts
@@ -2,9 +2,31 @@ import React from 'react';
 
 import { Size, BaseProps } from '../../types/common.types';
 
+/**
+ * Props do componente Avatar
+ * @example
+ * ```tsx
+ * <Avatar icon={<UserIcon />} size="lg" />
+ * ```
+ * @example
+ * ```tsx
+ * <Avatar 
+ *   icon={<ProfileIcon />} 
+ *   size="sm"
+ *   className="custom-avatar"
+ * />
+ * ```
+ */
 export interface AvatarProps {
+  /** ID único do elemento */
   id?: BaseProps['id'];
+  
+  /** Ícone ou conteúdo a ser exibido no avatar */
   icon: React.ReactNode;
+  
+  /** Tamanho do avatar */
   size?: Size;
+  
+  /** Classe CSS customizada */
   className?: BaseProps['className'];
 }
