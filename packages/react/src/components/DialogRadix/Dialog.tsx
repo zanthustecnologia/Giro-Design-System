2e03d509cb2890d842598deb5a4e2914dd7d567f
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-19 14:32:36 -0300

    refactor: Further adjustments to the CSS, added action buttons, and set the second button to only appear if it has text inside.

--- packages/react/src/components/DialogRadix/Dialog.tsx
+++ packages/react/src/components/DialogRadix/Dialog.tsx
@@ -1,8 +1,10 @@
 import { AlertDialog } from "radix-ui";
 import * as React from "react";
 
+
 import styles from './Dialog.module.scss';
 import { DialogRadixProps } from "./Dialog.types";
+import Button from '../Button/Button';
 
 const Dialog: React.FC<DialogRadixProps> = ({
   children,
@@ -25,11 +27,13 @@ const Dialog: React.FC<DialogRadixProps> = ({
 					{text}
 				</AlertDialog.Description>
 				<div style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}>
-					<AlertDialog.Cancel asChild>
-						<button className={styles.Button + ' ' + styles.mauve}>{textCancel}</button>
-					</AlertDialog.Cancel>
+					{!!(textCancel && textCancel.trim()) && (
+						<AlertDialog.Cancel asChild>
+							<Button variant="outlined">{textCancel}</Button>
+						</AlertDialog.Cancel>
+					)}
 					<AlertDialog.Action asChild>
-						<button className={styles.Button + ' ' + styles.red}>{textConfirm}</button>
+						<Button variant="filled">{textConfirm}</Button>
 					</AlertDialog.Action>
 				</div>
 			</AlertDialog.Content>
