1d0e9953e1f1670ca83b576be080519b5b08454f
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-20 10:56:12 -0300

    refactor: Applied commonTypes

--- packages/react/src/components/Dialog/Dialog.types.ts
+++ packages/react/src/components/Dialog/Dialog.types.ts
@@ -1,11 +1,10 @@
 import { ReactNode } from 'react';
+import { BaseProps } from '../../types/common.types';
 
-export interface DialogProps {
+export interface DialogProps extends BaseProps {
   children?: ReactNode;
   title?: string;
   text?: ReactNode;
   textConfirm?: string;
   textCancel?: string;
-  id?: string;
-  className?: string;
 }
