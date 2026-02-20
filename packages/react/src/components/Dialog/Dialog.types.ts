6bab08ccfc5eeb789e1ed49fe29160b52bbc8e85
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-02-20 10:59:25 -0300

    refactor: Applied spreadProps

--- packages/react/src/components/Dialog/Dialog.types.ts
+++ packages/react/src/components/Dialog/Dialog.types.ts
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react';
+
 import { BaseProps } from '../../types/common.types';
 
-export interface DialogProps extends BaseProps {
+export interface DialogProps extends BaseProps, React.HTMLAttributes<HTMLDivElement> {
   children?: ReactNode;
   title?: string;
   text?: ReactNode;
