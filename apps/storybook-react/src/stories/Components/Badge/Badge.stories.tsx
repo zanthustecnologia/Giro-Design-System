392ef9b8f77fb3c29d9730b151aad13bb8eccded
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-04-23 13:13:48 -0300

    refactor: Final adjustments to CSS and Storybook.

--- apps/storybook-react/src/stories/Components/Badge/Badge.stories.tsx
+++ apps/storybook-react/src/stories/Components/Badge/Badge.stories.tsx
@@ -48,7 +48,7 @@ const Anchor = () => (
 
 const Template: StoryFn<BadgeProps> = (args) => (
   <Badge {...args}>
-    <Avatar />
+    <Avatar src= 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80' />
   </Badge>
 );
 
