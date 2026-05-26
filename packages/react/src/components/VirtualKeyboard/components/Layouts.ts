ebc5aa65f645a46049af7af0604d1409019d9911
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-05-26 09:08:00 -0300

    refactor: Layout Adjustment

--- packages/react/src/components/VirtualKeyboard/components/Layouts.ts
+++ packages/react/src/components/VirtualKeyboard/components/Layouts.ts
@@ -19,7 +19,7 @@ export const NATIVE_LAYOUTS: Partial<Record<VirtualKeyboardLayout, Record<string
     caps: [
       '1 2 3 4 5 6 7 8 9 0',
       'Q W E R T Y U I O P {bksp}',
-      'A S D F G H J K L {enter}',
+      'A S D F G H J K L Ç {enter}',
       '{shiftactivated} Z X C V B N M , . {shiftactivated}',
       '{alt} {smileys} {space} {downkeyboard}',
     ],
