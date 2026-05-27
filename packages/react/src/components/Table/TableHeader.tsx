8cf0b86354f323dfe1afa3f911e723101e73ea57
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-05-27 00:35:26 -0300

    refactor: Adjustments to the Table in relation to changes in the Filter.

--- packages/react/src/components/Table/TableHeader.tsx
+++ packages/react/src/components/Table/TableHeader.tsx
@@ -19,7 +19,7 @@ interface BaseFilterItem {
 }
 
 interface CheckboxFilterItem extends BaseFilterItem {
-  type: 'multiple' | 'single' | 'icon';
+  type: 'multiple' | 'single';
   items: FilterListItem[];
   selectedIds?: string[];
   onSelectionChange?: (selectedIds: string[]) => void;
@@ -45,7 +45,7 @@ const isCalendarFilter = (filter: FilterItem): filter is CalendarFilterItem => {
 };
 
 const isCheckboxFilter = (filter: FilterItem): filter is CheckboxFilterItem => {
-  return filter.type === 'multiple' || filter.type === 'single' || filter.type === 'icon';
+  return filter.type === 'multiple' || filter.type === 'single';
 };
 
 export interface TableHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
