f87f94ddc33aa7976456f76e8590e1890c71f707
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-07-14 11:41:35 -0300

    refactor: reorganize imports in the content.config.ts file

--- apps/hub/src/content.config.ts
+++ apps/hub/src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { defineCollection } from 'astro:content';
+import { z } from 'zod';
 
 const blog = defineCollection({
   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
