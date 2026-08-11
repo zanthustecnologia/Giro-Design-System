1b08791770a90d6226b23aac2f82fcdf5a9d7a4e
Giovani Ramos <giovani.ramos@zanthus.com.br>
2026-08-11 15:39:36 -0300

    feat: adds styles and classes to the Tabs component

--- packages/react/src/components/Tabs/Tabs.tsx
+++ packages/react/src/components/Tabs/Tabs.tsx
@@ -1,6 +1,8 @@
+import clsx from "clsx";
 import { Tabs as TabsRadix } from "radix-ui";
 import * as React from "react";
 
+import styles from "./Tabs.module.scss";
 import { TabsProps } from "./Tabs.types";
 
 const Tabs: React.FC<TabsProps> = ({
@@ -26,15 +28,16 @@ const Tabs: React.FC<TabsProps> = ({
 		orientation={orientation}
 		activationMode={activationMode}
 		dir={dir}
-		className={className}
+		className={clsx(styles.Root, className)}
 		data-testid={testId}
 	>
-		<TabsRadix.List loop={loop} aria-label={ariaLabel}>
+		<TabsRadix.List loop={loop} aria-label={ariaLabel} className={styles.List}>
 			{items.map((item) => (
 				<TabsRadix.Trigger
 					key={item.value}
 					value={item.value}
 					disabled={disabled || item.disabled}
+					className={styles.Trigger}
 				>
 					{item.icon}
 					{item.label}
@@ -43,7 +46,7 @@ const Tabs: React.FC<TabsProps> = ({
 		</TabsRadix.List>
 
 		{items.map((item) => (
-			<TabsRadix.Content key={item.value} value={item.value}>
+			<TabsRadix.Content key={item.value} value={item.value} className={styles.Content}>
 				{item.content}
 			</TabsRadix.Content>
 		))}
